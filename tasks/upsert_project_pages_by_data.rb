cf31bff7c65eaedbb5066d10d18725b1465cd389
Yohei Yasukawa <yohei@yasslab.jp>
2022-12-22 22:24:02 +0900

    Fix wrong text message: 次 <---> 前 #115

--- tasks/upsert_project_pages_by_data.rb
+++ tasks/upsert_project_pages_by_data.rb
@@ -91,8 +91,8 @@ projects.each_with_index do |project, index|
       .nav a:link, .nav a:visited { color: white; }
     </style>
     <div style="display: table; border-collapse: separate; border-spacing: 15px 0; font-size: 70%; width: 100%; padding: 10px 10px; margin-top: 100px; background-color: rgb(40, 161, 58);">
-      <p class="nav prev">&larr; 次<br><a href='#{prev_project[:id]}'>#{ t.truncate(prev_project[:title]) }</a></p>
-      <p class="nav next">前 &rarr;<br><a href='#{next_project[:id]}'>#{ t.truncate(next_project[:title]) }</a></p>
+      <p class="nav prev">&larr; 前<br><a href='#{prev_project[:id]}'>#{ t.truncate(prev_project[:title]) }</a></p>
+      <p class="nav next">次 &rarr;<br><a href='#{next_project[:id]}'>#{ t.truncate(next_project[:title]) }</a></p>
     </div>
 
     {% include project-navigation.html %}
