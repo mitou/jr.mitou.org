19176e28f87b35ff56037c449def9c99b067b33b
Yohei Yasukawa <yohei@yasslab.jp>
2023-11-04 12:06:03 +0900

    Show 'ダウンロードする' button if a given project is linked to GitHub releases page

--- tasks/upsert_project_pages_by_data.rb
+++ tasks/upsert_project_pages_by_data.rb
@@ -38,7 +38,9 @@ projects.each_with_index do |project, index|
 
     <div class='flex'>
       {% if pj.link %}
-        {% if pj.link contains 'github.com' %}
+        {% if pj.link contains 'github.com' and pj.link contains 'releases' %}
+           <a href='{{ pj.link }}' target='_blank' class='button'>ダウンロードする</a>
+        {% elsif pj.link contains 'github.com' %}
            <a href='{{ pj.link }}' target='_blank' class='button'>ソースコードを見る</a>
         {% else %}
            <a href='{{ pj.link }}' target='_blank' class='button'>公式サイトを見る</a>
