af5e0b8b686f4d7c4eed6927763de1f2bed5efbb
yasulab <yasulab@users.noreply.github.com>
2023-12-28 05:06:47 +0000

    deploy: 399c71882b93f4f2b72a7f891f40020d92c5e827

--- tests/custom_checks.rb
+++ tests/custom_checks.rb
@@ -3,25 +3,40 @@
 
 # Check /**/*.json URLs return valid JSON data
 require 'json'
-class JSONs < ::HTMLProofer::Check
-  def valid_json?(filename)
-    JSON.load_file(filename)
-    true
-  rescue JSON::ParserError, TypeError => e
-    false
-  end
+class CustomChecks < ::HTMLProofer::Check
+  BASE_URL  = '_site/'
 
   def run
-    return unless @runner.current_filename == '_site/api.html'
+    check_json_apis if @runner.current_filename == '_site/api.html'
+    check_meta_tags if @runner.current_filename == '_site/stats.html'
+  end
+end
 
-    # Get JSON API URLs and check if they return valid JSON data.
-    @html.css('#index > ul > li').each do |node|
-      base_url  = '_site/'
-      json_path = node.at_css('a').attribute('href').value
-      # e.g. => /projects.json
+def check_meta_tags
+  # Check if meta tags render data correctly
+  @html.css('head > meta').each do |node|
+    if node.attribute('content') &&
+       node.attribute('content').value.include?('site.data')
 
-      next if valid_json?(base_url + json_path)
-      return add_failure("Invalid JSON format: #{json_path}")
+      add_failure("Failed to render Jekyll data: #{node.attribute('content')}")
     end
   end
+end
+
+def check_json_apis
+  # Get JSON API URLs and check if they return valid JSON data.
+  @html.css('#index > ul > li').each do |node|
+    json_path = node.at_css('a').attribute('href').value
+    # e.g. => /projects.json
+
+    next if valid_json?(BASE_URL + json_path)
+    add_failure("Invalid JSON format: #{json_path}")
+  end
+end
+
+def valid_json?(filename)
+  JSON.load_file(filename)
+  true
+rescue JSON::ParserError, TypeError => e
+  false
 end
