e56cece83b78b407697c0fd398fe920c2fc0a320
Yohei Yasukawa <yohei@yasslab.jp>
2023-12-28 14:29:35 +0900

    Run a custom check for all pages other than stats.html

--- tests/custom_checks.rb
+++ tests/custom_checks.rb
@@ -7,7 +7,7 @@ class CustomChecks < ::HTMLProofer::Check
   BASE_URL  = '_site/'
 
   def run
-    check_meta_tags if @runner.current_filename == '_site/stats.html'
+    check_meta_tags
     check_json_apis if @runner.current_filename == '_site/api.html'
   end
 end
