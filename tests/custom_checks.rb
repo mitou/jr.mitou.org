af070213aea9af07408c69e553b453ca55081dc1
yasulab <yasulab@users.noreply.github.com>
2024-05-12 11:56:37 +0000

    deploy: 789d6110520462afe4ab2c1faf8985bd53ca06b1

--- tests/custom_checks.rb
+++ tests/custom_checks.rb
@@ -56,7 +56,7 @@ def check_deadlines
     month_and_day = node.children.last.text.scan(/\d+月\d+日/).last
     next if month_and_day.nil? # 〆切の無い heading は省略
 
-    # 〆切のある heading の日付（後半の終端日分）が時間軸に沿っているかチェックする
+    # 〆切のある heading の日付（後半の終端日）が時間軸に沿っているかチェックする
     # 例: "3. 応募フォームから提案書をアップロードする （2024年4月6日 23:59まで）"
     # 例: "6. 追加インタビュー期間 （2024年5月14日〜5月27日）"
     this_deadline = "#{this_year}-%02d-%02d" % month_and_day.scan(/\d+/)
