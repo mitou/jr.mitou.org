e246fee0680c7aae2515c28ff40e423cd62f8e19
Yohei Yasukawa <yohei@yasslab.jp>
2024-08-25 11:51:41 +0900

    Update task to output English projects via projects.yml data

--- tasks/upsert_project_pages_by_data_en.rb
+++ tasks/upsert_project_pages_by_data_en.rb
@@ -4,10 +4,13 @@ require 'yaml'
 require 'truncato'
 
 # Remove existing files and re-generate them
-Dir.glob("./_posts/*.md").each { |filename| File.delete(filename) }
+Dir.glob("./_posts/english/*.md").each { |filename| File.delete(filename) }
 
 projects = YAML.load_file("_data/projects.yml", symbolize_names: true)
 projects.each_with_index do |project, index|
+  # To inspect & debug specific projects
+  #next unless project[:year] == 2017
+
   # Prev/Next project data for navigation
   this_year_projects = projects.select{|pj| pj[:year] == project[:year] }
   project_index = this_year_projects.index(project)
@@ -15,83 +18,88 @@ projects.each_with_index do |project, index|
   next_project  = this_year_projects.rotate(project_index + 1).first
 
   # Generate individual project page by data
-  path = "./_posts/#{project[:year]}-09-01-#{project[:id]}.md"
+  path = "./_posts/english/#{project[:year]}-09-01-#{project[:id]}.md"
   page = <<~PROJECT_PAGE
     ---
+    lang: en
     layout: post
-    title: "#{project[:title]}"
-    permalink: /projects/#{project[:year]}/#{project[:id]}
+    title: |
+      #{project[:title_en]}
+    description: |
+      #{project[:description_en]}
+    permalink: /english/projects/#{project[:year]}/#{project[:id]}
     thumbnail: /assets/img/projects/#{project[:year]}/#{project[:thumbnail]}
-    description: "#{project[:description]}"
     twitter_card: summary_large_image
     ---
 
     {% assign pj = site.data.projects | where_exp: "pj", "pj.id == '#{project[:id]}'" | first %}
 
     <div style='margin-top: 50px; margin-bottom: 30px;'>
-      <img class='top-img lazyload' src='/assets/img/spinner.svg' alt='サムネイル画像 - {{ pj.title }}'
+      <img class='top-img lazyload' src='/assets/img/spinner.svg' alt='Thumbnail - {{ pj.title }}'
       {% if pj.thumbnail %}    data-src='/assets/img/projects/{{ pj.year }}/{{ pj.thumbnail }}'
       {% else %}               data-src='/assets/img/projects/tbu.webp'
       {% endif %}                 title='{{ pj.title }}' style='border-radius: 6px;' loading='lazy' />
     </div>
 
-    {{ pj.description }}
+    {{ pj.description_en }}
 
     <div class='flex'>
       {% if pj.link %}
         {% if pj.link contains 'github.com' and pj.link contains 'releases' %}
-           <a href='{{ pj.link }}' target='_blank' class='button'>ダウンロードする</a>
+           <a href='{{ pj.link }}' target='_blank' class='button'>Donwload codes on GitHub</a>
         {% elsif pj.link contains 'github.com' %}
-           <a href='{{ pj.link }}' target='_blank' class='button'>ソースコードを見る</a>
+           <a href='{{ pj.link }}' target='_blank' class='button'>View project on GitHub</a>
         {% else %}
-           <a href='{{ pj.link }}' target='_blank' class='button'>公式サイトを見る</a>
+           <a href='{{ pj.link }}' target='_blank' class='button'>View official website</a>
         {% endif %}
       {% endif %}
 
       <a href="https://twitter.com/intent/tweet?text={{ pj.title }}&via=MitouJr&hashtags=未踏ジュニア{% if pj.tags %},{{ pj.tags | join: ','}}{% endif %}&related=MitouJr&lang=jp&url={{ site.url }}/projects/{{ pj.year }}/{{ pj.id }}" class="button" target="_blank" rel="noopener">ツイートする</a>
     </div>
 
-    ### クリエータ {#creator}
+    ### Creator {#creator}
     <p>
       {% for creator_id in pj.creator_ids %}
         {% include creator.html is_simple=true %}
       {% endfor %}
-      <small>(<a href='/projects/{{ pj.year }}'>{{ pj.year }}年度</a> 採択 / {% include link-to-mentor.html id=pj.mentor_id %}PM)</small>
+      <small>(Year: <a href='/projects/{{ pj.year }}'>{{ pj.year }}</a> / Mentor: {% include link-to-mentor.html id=pj.mentor_id %})</small>
     </p>
 
+    {% comment %}
     {% if pj.comment %}
-    ### PMコメント {#comment}
+    ### Mentor's Comment {#comment}
     <p class="project-comment">{{ pj.comment }}</p>
     {% endif %}
+    {% endcomment %}
 
     {% if pj.promotion %}
     {% if pj.promotion contains '.gif' %}
-    ## デモ動画 {#demo}
+    ## Demo {#demo}
     <img class='top-img lazyload' src='/assets/img/spinner.svg' alt='デモ動画 (Gif)'
          data-src='/assets/img/projects/{{ pj.year }}/{{ pj.promotion }}' loading='lazy'
          style='margin-bottom: 10px; border-radius: 6px;' />
     {% else %}
-    ## デモ動画 {#demo}
+    ## Demo {#demo}
     <div class="youtube">
       <iframe width="560" height="315" class="lazyload" data-src="https://www.youtube.com/embed/{{ pj.promotion }}?rel=0" frameborder="0" allowfullscreen=""></iframe>
     </div>
-    <a href="https://youtu.be/{{ pj.promotion }}" target="_blank" rel="noopener" class="button">YouTube で見る</a>
+    <a href="https://youtu.be/{{ pj.promotion }}" target="_blank" rel="noopener" class="button">Watch on YouTube</a>
     {% endif %}
     {% endif %}
 
     {% if pj.final %}
-    ## 発表動画 {#final}
+    ## Pitch {#final}
     <div class="youtube">
       <iframe width="560" height="315" class="lazyload" data-src="https://www.youtube.com/embed/{{ pj.final }}?rel=0{% if pj.final_start %}&start={{ pj.final_start }}{% endif %}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
     </div>
-    <a href="https://youtu.be/{{ pj.final }}{% if pj.final_start %}?t={{ pj.final_start }}{% endif %}" target="_blank" rel="noopener" class="button">YouTube で見る</a>
+    <a href="https://youtu.be/{{ pj.final }}{% if pj.final_start %}?t={{ pj.final_start }}{% endif %}" target="_blank" rel="noopener" class="button">Watch on YouTube</a>
     {% endif %}
 
     <nav>
-      <p class='nav prev'><a href='#{prev_project[:id]}' title='#{prev_project[:title]}'>&larr; 前<br>
-        #{ Truncato.truncate(prev_project[:title]) }</a></p>
-      <p class='nav next'><a href='#{next_project[:id]}' title='#{next_project[:title]}'>次 &rarr;<br>
-        #{ Truncato.truncate(next_project[:title]) }</a></p>
+      <p class='nav prev'><a href='#{prev_project[:id]}' title='#{prev_project[:title_en]}'>&larr; Prev<br>
+        #{ Truncato.truncate(prev_project[:title_en]) }</a></p>
+      <p class='nav next'><a href='#{next_project[:id]}' title='#{next_project[:title_en]}'>Next &rarr;<br>
+        #{ Truncato.truncate(next_project[:title_en]) }</a></p>
     </nav>
 
     {% include project-navigation.html %}
