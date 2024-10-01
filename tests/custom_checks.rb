babac39fdb09e7c6eee47ee09cef810d52dcd923
Yohei Yasukawa <yohei@yasslab.jp>
2024-10-01 21:07:28 +0900

    Add test: Raise error if given thumbnail file is 404

--- tests/custom_checks.rb
+++ tests/custom_checks.rb
@@ -4,79 +4,93 @@
 require 'json'
 
 class CustomChecks < ::HTMLProofer::Check
+  BASE_PATH = '_site'
+
   def run
     current_filename = @runner.current_filename
     puts "\tchecking ... " + current_filename[5..].split('.').first
 
     check_meta_tags
-    check_json_apis if valid_and_equal_to? '_site/apis.html'
-    check_deadlines if valid_and_equal_to? '_site/guideline.html'
+    check_json_apis if valid_and_equal_to?(BASE_PATH + '/apis.html')
+    check_deadlines if valid_and_equal_to?(BASE_PATH + '/guideline.html')
   end
-end
 
-def valid_and_equal_to?(filename)
-  # TODO: This should call add_failure() once at maximum but better than ignoring invalid filename.
-  self.add_failure("No such file found: #{filename}") if not File.exist?(filename)
-  self.add_failure("No redirect file: #{filename} is redirect file") if File.read(filename).include? 'redirect_to'
+  def valid_and_equal_to?(filename)
+    # TODO: This should call add_failure() once at maximum but better than ignoring invalid filename.
+    self.add_failure("No such file found: #{filename}") if not File.exist?(filename)
+    self.add_failure("No redirect file: #{filename} is redirect file") if File.read(filename).include? 'redirect_to'
+    @runner.current_filename == filename ? true : false
+  end
 
-  @runner.current_filename == filename ? true : false
-end
+  # Check if meta tags render data correctly
+  # Example: https://jr.mitou.org/stats
+  def check_meta_tags
+    @html.css('head > meta').each do |node|
+      if node.attribute('content') &&
+         node.attribute('content').value.include?('site.data')
 
-# Check if meta tags render data correctly
-# Example: https://jr.mitou.org/stats
-def check_meta_tags
-  @html.css('head > meta').each do |node|
-    if node.attribute('content') &&
-       node.attribute('content').value.include?('site.data')
-
-      add_failure("Failed to render Jekyll data: #{node.attribute('content')}")
+        add_failure("Failed to render Jekyll data: #{node.attribute('content')}")
+      end
     end
   end
-end
 
-# Get JSON API URLs from https://jr.mitou.org/apis
-# and check if they all return valid JSON data.
-def check_json_apis
-  @html.css('#index > ul > li').each do |node|
-    json_path = node.at_css('a').attribute('href').value
-    # e.g. => /projects.json
+  # Get JSON API URLs from https://jr.mitou.org/apis
+  # and check if they all return valid JSON data.
+  def check_json_apis
+    @html.css('#index > ul > li').each do |node|
+      json_path = node.at_css('a').attribute('href').value
+      # e.g. => /projects.json
 
-    add_failure("Invalid JSON format: #{json_path}") if not valid_json?('_site' + json_path)
+      add_failure("Invalid JSON format: #{json_path}") if not valid_json?(BASE_PATH + json_path)
+
+      # If given JSON is valid, check columns in it
+      responses  = JSON.load_file(BASE_PATH + json_path, symbolize_names: true)
+      responses.map{|response| response[:thumbnail] }.compact.each do |thumbnail|
+        thumbnail.gsub!('https://jr.mitou.org', BASE_PATH)
+
+        add_failure(
+          <<~ERROR_MESSAGE
+            No such thumbnail: #{thumbnail}
+            \s API Endpoint: #{BASE_PATH + json_path}
+          ERROR_MESSAGE
+        ) if not File.exist? thumbnail
+      end
+    end
   end
-end
 
-def valid_json?(filename)
-  JSON.load_file(filename)
-  true
-rescue JSON::ParserError, TypeError => e
-  false
-end
+  def valid_json?(filename)
+    JSON.load_file(filename)
+    true
+  rescue JSON::ParserError, TypeError => e
+    false
+  end
 
-# Check proceeding schedule is correct in time order:
-# e.g. https://github.com/mitou/jr.mitou.org/pull/180
-def check_deadlines
-  this_year     = Date.today.year
-  prev_text     = ''
-  prev_deadline = "#{this_year}-01-23"
-  this_deadline = "#{this_year}-01-24"
+  # Check proceeding schedule is correct in time order:
+  # e.g. https://github.com/mitou/jr.mitou.org/pull/180
+  def check_deadlines
+    this_year     = Date.today.year
+    prev_text     = ''
+    prev_deadline = "#{this_year}-01-23"
+    this_deadline = "#{this_year}-01-24"
 
-  # Fetch heading nodes like "1. プロジェクトの計画を立てる"
-  @html.search('h3').select{|n| n.text.start_with?(/\d\./)}.each do |node|
-    month_and_day = node.children.last.text.scan(/\d+月\d+日/).last
-    next if month_and_day.nil? # 〆切の無い heading は省略
+    # Fetch heading nodes like "1. プロジェクトの計画を立てる"
+    @html.search('h3').select{|n| n.text.start_with?(/\d\./)}.each do |node|
+      month_and_day = node.children.last.text.scan(/\d+月\d+日/).last
+      next if month_and_day.nil? # 〆切の無い heading は省略
 
-    # 〆切のある heading の日付（後半の終端日）が時間軸に沿っているかチェックする
-    # 例: "3. 応募フォームから提案書をアップロードする （2024年4月6日 23:59まで）"
-    # 例: "6. 追加インタビュー期間 （2024年5月14日〜5月27日）"
-    this_deadline = "#{this_year}-%02d-%02d" % month_and_day.scan(/\d+/)
-    add_failure(
-      <<~ERROR_MESSAGE
-        This deadline would be inconsistent with previous one:
-          \s prev_deadline: #{prev_text}
-          \s this_deadline: #{node.text}
-      ERROR_MESSAGE
-    ) if prev_deadline > this_deadline
-    prev_deadline = this_deadline
-    prev_text     = node.text
+      # 〆切のある heading の日付（後半の終端日）が時間軸に沿っているかチェックする
+      # 例: "3. 応募フォームから提案書をアップロードする （2024年4月6日 23:59まで）"
+      # 例: "6. 追加インタビュー期間 （2024年5月14日〜5月27日）"
+      this_deadline = "#{this_year}-%02d-%02d" % month_and_day.scan(/\d+/)
+      add_failure(
+        <<~ERROR_MESSAGE
+          This deadline would be inconsistent with previous one:
+            \s prev_deadline: #{prev_text}
+            \s this_deadline: #{node.text}
+        ERROR_MESSAGE
+      ) if prev_deadline > this_deadline
+      prev_deadline = this_deadline
+      prev_text     = node.text
+    end
   end
 end
