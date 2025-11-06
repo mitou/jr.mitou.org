788c8962e2edaac1806df225001a5dc74b89644f
Yohei Yasukawa <yohei@yasslab.jp>
2025-11-06 20:16:08 +0900

    Partialize YouTube Embed (iframe) section: /applications/*

--- tasks/upsert_project_samples_by_data.rb
+++ tasks/upsert_project_samples_by_data.rb
@@ -45,7 +45,7 @@ project_samples.each_with_index do |project, index|
 
     {% assign pj = site.data.projects | where_exp: "pj", "pj.id == '#{project[:id]}'" | first %}
     <div class="youtube">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/{{ pj.final }}?rel=0{% if pj.final_start %}&start={{ pj.final_start }}{% endif %}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      {% include youtube-embed.html id=pj.final start=pj.final_start %}
     </div>
 
     <div class='flex'>
