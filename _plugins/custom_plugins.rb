0cbf29e347cab4b8739eb16889163a7d4f8b224c
Yohei Yasukawa <yohei@yasslab.jp>
2025-11-09 13:49:08 +0900

    fix: Jekyll APIジェネレーターの実行タイミング問題を修正して2回ビルドを不要に
    - site.pagesからJSON URLを直接収集（_siteファイル読み取りの代替）
    - GitHub Actionsの重複ビルドコマンドを削除
    - Generator優先度を:lowに変更（:lowestから）
    
    詳細はPR説明を参照

--- _plugins/custom_plugins.rb
+++ _plugins/custom_plugins.rb
@@ -28,32 +28,38 @@ end
 # Register LiquifyFilter module to Jekyll template
 Liquid::Template.register_filter(Jekyll::LiquifyFilter)
 
-# A plugin to set custom site.data like APIs: {% site.data.apis %}
-# https://jekyllrb.com/docs/plugins/generators/
+# See PR description for technical details and rationale
 module Reading
   class Generator < Jekyll::Generator
-    # This plugin needs _site/**/*.json files.
-    # So this needs to be execed at very last.
-    priority :lowest
+    priority :low
     safe true
 
-    # Tweak data that Jekyll can access like `{% site.data.foobars %}`
     def generate(site)
       apis = []
-      #binding.irb; exit
-      Dir.glob('./**/*.json') do |filepath|
-        next unless filepath.start_with? './_site' # Check only generated *.json pages
-        filepath = filepath[7..] # Correct path: ./_site/api.json -> /api.json
-
-        # NOTE: Can't get data if set like `key: value`
-        #       It seems needed to be like `'key' => 'value'`
-        apis << {
-          'to_json' => filepath,      # => /api.json
-          'to_html' => filepath[..-6] # => /api
-        }
+      
+      site.pages.each do |page|
+        if page.url.end_with?('.json')
+          next if page.url.start_with?('/assets/')
+          
+          apis << {
+            'to_json' => page.url,
+            'to_html' => page.url[..-6]
+          }
+        end
       end
-
-      # Now you can get data above by like `{{ site.data.apis | first }}`
+      
+      site.collections.each do |_name, collection|
+        collection.docs.each do |doc|
+          if doc.url && doc.url.end_with?('.json')
+            apis << {
+              'to_json' => doc.url,
+              'to_html' => doc.url[..-6]
+            }
+          end
+        end
+      end
+      
+      apis.sort_by! { |api| api['to_json'] }
       site.data['apis'] = apis
     end
   end
