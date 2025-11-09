1441d49413504fa1c0ac3c6cec03855d22855acc
Yohei Yasukawa <yohei@yasslab.jp>
2025-11-09 14:05:24 +0900

    refactor: Jekyll APIジェネレーターをより関数型プログラミング的なアプローチに改善
    - each/if文をselect/mapチェーンに変更（宣言的コード）
    - flat_mapでcollections処理を簡潔化
    - delete_suffix('.json')で意図を明確化（[..-6]の代替）
    - 変数名を改善（json_docs → json_posts）
    - コメントで処理目的を明確化

--- _plugins/custom_plugins.rb
+++ _plugins/custom_plugins.rb
@@ -29,37 +29,36 @@ end
 Liquid::Template.register_filter(Jekyll::LiquifyFilter)
 
 # See PR description for technical details and rationale
+# https://github.com/mitou/jr.mitou.org/pull/239
 module Reading
   class Generator < Jekyll::Generator
     priority :low
     safe true
 
     def generate(site)
-      apis = []
-      
-      site.pages.each do |page|
-        if page.url.end_with?('.json')
-          next if page.url.start_with?('/assets/')
-          
-          apis << {
-            'to_json' => page.url,
-            'to_html' => page.url[..-6]
+      # For general pages like /index.md
+      json_pages = site.pages
+        .select { |page| page.url.end_with?('.json') && !page.url.start_with?('/assets/') }
+        .map    { |page|
+        {
+          'to_json' => page.url,
+          'to_html' => page.url.delete_suffix('.json')
+        }
+      }
+
+      # For project pages like /projects/YYYY/project_name.json
+      json_posts = site.collections.flat_map do |_name, collection|
+        collection.docs
+          .select { |doc| doc.url && doc.url.end_with?('.json') }
+          .map    { |doc|
+          {
+            'to_json' => doc.url,
+            'to_html' => doc.url.delete_suffix('.json')
           }
-        end
+        }
       end
-      
-      site.collections.each do |_name, collection|
-        collection.docs.each do |doc|
-          if doc.url && doc.url.end_with?('.json')
-            apis << {
-              'to_json' => doc.url,
-              'to_html' => doc.url[..-6]
-            }
-          end
-        end
-      end
-      
-      apis.sort_by! { |api| api['to_json'] }
+
+      apis = (json_pages + json_posts).sort_by { |api| api['to_json'] }
       site.data['apis'] = apis
     end
   end
